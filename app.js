bdbef6fc2077cba9dcf4a4f22d6445d94d663516
Katelyn Michalove <katelynmichalove@gmail.com>
2018-11-16 15:23:40 -0800

    checkpoint 6

---
+++ app.js
@@ -0,0 +1,67 @@
+function onReady() {
+  let toDos = [];
+  const addToDoForm = document.getElementById('addToDoForm');
+  let id = 0;
+
+  function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
+    if (!newToDoText.value) { return; }
+
+    toDos.push ({
+      title: newToDoText.value,
+      complete: false,
+      id: id
+
+    });
+
+    id++;
+
+    newToDoText.value = '';
+
+    renderTheUI();
+  }
+
+  function deleteToDo(id) {
+    return toDos.filter(toDo => toDo.id !==id);
+  }
+
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'x';
+
+      deleteButton.addEventListener('click', event => {
+        toDos = deleteToDo(toDo.id);
+        renderTheUI();
+      });
+
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+      newLi.appendChild(deleteButton);
+
+    });
+  }
+
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+    createNewToDo();
+  });
+
+  renderTheUI();
+}
+
+
+window.onload = function () {
+  alert("The window has loaded!");
+  onReady();
+};
